883b57b2b34c912b09bea1511f84dc21e121a03d
Daniel Eshanokpe <eshanokpe@gmail.com>
2026-04-07 14:24:57 +0100

    upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-}
+    id("com.google.gms.google-services") 
+} 
 
 android {
     namespace = "com.example.flutter_email_client"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30.0.14904198"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_email_client"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -37,7 +35,7 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
+} 
 
 flutter {
     source = "../.."
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,19 @@
+buildscript {
+    extra.apply {
+        set("kotlin_version", "2.1.0")
+    }
+    repositories {
+        
+        google()
+        mavenCentral()
+    }
+    
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
+        classpath("com.google.gms:google-services:4.4.4")
+    }
+}
 allprojects {
     repositories {
         google()
